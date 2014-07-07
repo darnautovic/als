726eac673f61a858f6194d3d1a1a5d931146d74f
Damir Arnautovic <darnauto@fesb.hr>
2014-07-07 11:35:07 +0200

    fixed 1.sql evolution script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -44,7 +44,7 @@ CREATE TABLE clients (
    password             VARCHAR(64) NOT NULL,
    first_name           VARCHAR(64) NOT NULL,
    last_name            VARCHAR(64) NOT NULL,
-   email                VARCHAR(64) NOT NULL,
+   email                VARCHAR(64) NOT NULL
 );
 
 # --- !Downs
